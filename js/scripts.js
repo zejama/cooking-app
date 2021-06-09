9a74cfd2504922f68de35a2d18d3b5aa0c69640d
Zeinab Jama <zejama99@gmail.com>
2021-06-08 20:24:27 -0700

    add functions for 4 different units of measurements

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,27 @@
 function convertF(number1) {
 	return (5/9) * (number1-3);
-}    
-  
-const number1 = parseInt(prompt("What is the temp in Fahrenheit?"));
-  
-alert(convertF(number1));
+}   // TO CELCIUS    
 
+function convertQuarts(number1) {
+
+  return number1 * 2;
+
+} // TO PINTS
+
+function convertPints(number1) {
+
+  return number1 * 2;
+
+} // TO CUPS
+
+function convertQuartsToGallons(number1) {
+
+  return number1 / 4
+
+} // TO GALLONS
+
+const number1 = parseInt(prompt("Enter the number you want to convert: "));
+
+const result = convertQuartsToGallons (number1);
+
+alert(result);
